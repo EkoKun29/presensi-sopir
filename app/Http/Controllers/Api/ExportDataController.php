258fb0263c1a2119e464d80140e9fe055ec4c14c
EkoKun29 <ekoseptyonugroho9@.com>
2024-11-02 14:39:46 +0700

    fetch:presensi

--- app/Http/Controllers/Api/ExportDataController.php
+++ app/Http/Controllers/Api/ExportDataController.php
@@ -1,15 +1,15 @@
 <?php
+
 namespace App\Http\Controllers\API;
+
 use App\Http\Controllers\Controller;
-use App\Models\DataKarywan;
-use Illuminate\Http\Request;
 use App\Models\Presensi;
+use Illuminate\Http\Request;
 
 class ExportDataController extends Controller
 {
     public function presensi($startDate, $endDate){
-        $presensi = Presensi::whereBetween('created_at', [$startDate, $endDate])->get();
-        return response()->json($presensi);
+        $rekap = Presensi::whereBetween('created_at', [$startDate, $endDate])->get();
+        return response()->json($rekap);
     }
-    
 }
