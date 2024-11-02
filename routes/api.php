12d211944245b5ad0c1c1dd736872cdcd8501910
EkoKun29 <ekoseptyonugroho9@.com>
2024-11-02 13:57:11 +0700

    fix:api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ExportDataController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/presensi/{startDate}/{endDate}', [App\Http\Controllers\API\ExportDataController::class, 'presensi']);
+Route::get('/presensi/{startDate}/{endDate}', [ExportDataController::class, 'presensi']);
